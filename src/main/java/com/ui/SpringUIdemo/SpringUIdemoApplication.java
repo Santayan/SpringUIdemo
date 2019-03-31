302098707a83317477166555d9bead0af1048ab4
Santayan <santayan.paul@gmail.com>
2019-03-31 09:57:09 -0400

    Initial commit

---
+++ src/main/java/com/ui/SpringUIdemo/SpringUIdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ui.SpringUIdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUIdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUIdemoApplication.class, args);
+	}
+
+}
